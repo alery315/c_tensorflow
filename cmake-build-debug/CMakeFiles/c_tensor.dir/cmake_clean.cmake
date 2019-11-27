27355ff09cf5382aa484e225d1d01b577b2ba14a
alery315 <sample29901@gmail.com>
2019-11-27 10:42:47 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/c_tensor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_tensor.dir/use_pb.c.o"
+  "c_tensor.pdb"
+  "c_tensor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_tensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/c_tensorflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_tensorflow.dir/use_pb.c.o"
+  "c_tensorflow.pdb"
+  "c_tensorflow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_tensorflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
